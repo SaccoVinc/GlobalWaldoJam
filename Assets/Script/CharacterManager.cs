cc499563d144e5cd5d33d2ddddeae3d69763c4a0
LucaPotenza <99011451+LucaPotenza@users.noreply.github.com>
2026-01-31 16:29:35 +0100

    character movement

--- Assets/Script/CharacterManager.cs
+++ Assets/Script/CharacterManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,24 +10,27 @@ public class CharacterManager : MonoBehaviour
     [SerializeField] int quantity;
 
     private static List<GameObject> _characters = new List<GameObject>();
+    private static Camera _camera;
+    private static Vector3 _border;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Camera camera = Camera.main;
-        Vector2 border = new Vector2(camera.orthographicSize * camera.aspect, camera.orthographicSize);
+        _camera = Camera.main;
+        _border = new Vector2(_camera.orthographicSize * _camera.aspect, _camera.orthographicSize);
         
         for (int i = 0; i < quantity; i++)
         {
             Vector3 position = new Vector3
             {
-                x = Random.Range(-border.x, border.x),
-                y = Random.Range(-border.y, border.y),
+                x = Random.Range(-_border.x, _border.x),
+                y = Random.Range(-_border.y, _border.y),
                 z = 0
             };
             GameObject characterInstance = Instantiate(character, position, Quaternion.identity) as GameObject;
             characterInstance.transform.parent = transform;
+            MoveInstance(characterInstance);
             _characters.Append(characterInstance);
         }
     }
@@ -39,7 +43,24 @@ public class CharacterManager : MonoBehaviour
 
     void MoveInstance(GameObject characterInstance)
     {
+        Vector3 newPosition = new Vector3
+        {
+            x = Random.Range(-_border.x, _border.x),
+            y = Random.Range(-_border.y, _border.y),
+            z = 0
+        };
+        characterInstance.transform.DOMove(newPosition, Random.Range(3.0f, 10.0f), true).OnComplete( 
+            () => WaitInstance(characterInstance)
+        );
         
+        DOTween.Play(characterInstance);
+    }
+
+    IEnumerable<WaitForSeconds> WaitInstance(GameObject characterInstance)
+    {
+        Debug.Log("Waiting " + characterInstance.name);
+        DOTween.Kill(characterInstance);
+        yield return new WaitForSeconds(Random.Range(0.5f, 1.0f));
+        MoveInstance(characterInstance);
     }
 }
-
